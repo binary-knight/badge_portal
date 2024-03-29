5e45c889c9e2027970d4fef85c91fe6f7bc17d7d
binaryknight <jason.s.knight@gmail.com>
2024-03-28 20:22:03 -0500

    adding deletion verification

--- src/DeleteComponent.js
+++ src/DeleteComponent.js
@@ -3,6 +3,11 @@ import { remove } from 'aws-amplify/storage';
 
 function DeleteComponent({ filename, onDeleteSuccess }) {
   const handleDelete = async () => {
+    const confirmDelete = window.confirm(`Are you sure you want to delete ${filename}`);
+    if (!confirmDelete) {
+      return;
+    }
+
     try {
       await remove({ key: filename });
       // Call the onDeleteSuccess callback to notify the parent component
